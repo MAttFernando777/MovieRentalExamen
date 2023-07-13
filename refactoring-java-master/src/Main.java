1c0d4de201f83d722c98325b17f126f76ec43b5a
MAttFernando777 <76874619+MAttFernando777@users.noreply.github.com>
2023-07-13 15:49:36 -0500

    Update Main.java

--- refactoring-java-master/src/Main.java
+++ refactoring-java-master/src/Main.java
@@ -1,6 +1,11 @@
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+package MovieRental;
 
 public class Main {
+  private static final Logger logger = Logger.getLogger(Main.class.getName());
 
   public static void main(String[] args) {
     String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
@@ -8,9 +13,10 @@ public class Main {
     String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
 
     if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+      logger.log(Level.SEVERE, "Expected: {0}{1}{1}Got: {2}", new Object[] { expected, System.lineSeparator(), result });
+      throw new AssertionError("Test failed. See logs for details.");
     }
 
-    System.out.println("Success");
+    logger.log(Level.INFO, "Success");
   }
 }
