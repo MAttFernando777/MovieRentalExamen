c57df946dacfc54ad1abb649656c9a1cd96ed40e
MAttFernando777 <76874619+MAttFernando777@users.noreply.github.com>
2023-07-13 15:49:18 -0500

    Update Customer.java

--- refactoring-java-master/src/Customer.java
+++ refactoring-java-master/src/Customer.java
@@ -1,11 +1,14 @@
+
+
 import java.util.List;
+
+package MovieRental;
 
 public class Customer {
     private String name;
     private List<MovieRental> rentals;
 
     public Customer(String name, List<MovieRental> rentals) {
-        this.name = name;
         this.rentals = rentals;
     }
 
